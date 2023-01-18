f3709917c63c6dd6335d43c491c91c4c48d3c293
ChangSoo Kim <92871579+soochang0812@users.noreply.github.com>
2023-01-18 21:14:12 +0900

    Create main.c

---
+++ scserver/src/proc/socket/main.c
@@ -0,0 +1,10 @@
+/**************************************************************************
+* Filename : main.c
+* Author : Chang Soo Kim <soochang0812@gmail.com>
+* description : 데이터 전송을 위한 Socket Server File
+**************************************************************************/
+#include <>
+
+int main (int argc. char* argv[]){
+    return 0;
+    }

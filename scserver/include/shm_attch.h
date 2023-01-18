df35fcc71ef07b5a09cf8c0c0fe5480e10809d00
ChangSoo Kim <92871579+soochang0812@users.noreply.github.com>
2023-01-18 21:12:37 +0900

    Update shm_attch.h

--- scserver/include/shm_attch.h
+++ scserver/include/shm_attch.h
@@ -1,8 +1,7 @@
 /**************************************************************************
+* Filename : shm_attch.h
 * Author : Chang Soo Kim <soochang0812@gmail.com>
 * description : Shared memory attach를 위한 header 파일
-* Arguments : NONE
-* Return : NONE
 **************************************************************************/
 #ifndef     __SHM_ATTCH_H
 #define     __SHM_ATTCH_H

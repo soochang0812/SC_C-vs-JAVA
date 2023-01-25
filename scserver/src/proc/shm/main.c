1f632d7a5d57077e2ece20af7f9f6d09c4609d02
ChangSoo Kim <92871579+soochang0812@users.noreply.github.com>
2023-01-25 16:16:11 +0900

    Create main.c

---
+++ scserver/src/proc/shm/main.c
@@ -0,0 +1,11 @@
+
+/**************************************************************************
+* Filename : main.c
+* Author : Chang Soo Kim <soochang0812@gmail.com>
+* description : 공유메모리 생성을 위한 SHM File
+**************************************************************************/
+#include <>
+
+int main (int argc. char* argv[]){
+    return 0;
+    }
